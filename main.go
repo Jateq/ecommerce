8b8030643bab7ff9e514e8cb07501d7bf3a85644
Jateq <temirlan.eraly1@gmail.com>
2023-04-25 02:21:47 +0600

    First step

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/Jateq/ecommerce/controllers"
+	"github.com/Jateq/ecommerce/database"
+	"github.com/Jateq/ecommerce/middleware"
+	"github.com/Jateq/ecommerce/routes"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/remove", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Jateq/ecommerce/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.AddProductAdmin())
+	incomingRoutes.GET("/users/productview", controllers.AllProducts())
+	incomingRoutes.GET("/users/search", controllers.SearchProduct())
+}
