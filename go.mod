5d6d8eca2624dd4dd9e16ffd435846017f4ea01a
Jateq <temirlan.eraly1@gmail.com>
2023-04-27 00:40:41 +0600

    finished models
    ayo

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/Jateq/ecommerce
 go 1.19
 
 require (
+	github.com/gin-gonic/gin v1.9.0
+	go.mongodb.org/mongo-driver v1.11.4
+)
+
+require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
--- models/models.go
+++ models/models.go
@@ -1,22 +1,64 @@
 package models
 
 import (
-	_ "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type User struct {
-	ID
-	FirstName
-	LastName
-	Password
-	Email
-	Phone
-	Token
-	RefreshToken
-	CreatedAt
-	UpdatedAt
-	UserID
-	UserCart
-	AddresDetails
-	OrderStatus
+	//actually go doesn't have support of json without this ` ` symbols
+	// and also _id is for mongoDB because it stores like that
+	// bson because we are referring to other structs
+	ID             primitive.ObjectID `json:"_id" bson:"_id"`
+	FirstName      *string            `json:"first_name" validate:"required,min=2,max=30"'`
+	LastName       *string            `json:"last_name" validate:"required, min=2,max=30"`
+	Password       *string            `json:"password" validate:"required, min=6"`
+	Email          *string            `json:"email" validate:"email, required"`
+	Phone          *string            `json:"phone" validate:"required"`
+	Token          *string            `json:"token"`
+	RefreshToken   *string            `json:"refresh_token"`
+	CreatedAt      time.Time          `json:"created_at"`
+	UpdatedAt      time.Time          `json:"updated_at"`
+	UserID         string             `json:"user_id"`
+	UserCart       []ProductUser      `json:"usercart" bson:"usercart"`
+	AddressDetails []Address          `json:"address" bson:"address"`
+	OrderStatus    []Order            `json:"order_status" bson:"order"`
+}
+
+type Product struct {
+	ProductID   primitive.ObjectID `bson:"_id"`
+	ProductName *string            `json:"product_name"`
+	Price       *uint64            `json:"price"`
+	Rating      *uint8             `json:"rating"`
+	Image       *string            `json:"image"`
+}
+
+type ProductUser struct {
+	ProductID   primitive.ObjectID `bson:"_id"`
+	ProductName *string            `json:"product_name" bson:"product_name"`
+	Price       int                `json:"price" bson:"price"`
+	Rating      *uint              `json:"rating" bson:"rating"`
+	Image       *string            `json:"image" bson:"image"`
+}
+
+type Address struct {
+	AddressID primitive.ObjectID `bson:"_id"`
+	House     *string            `json:"house_name" bson:"house_name"`
+	Street    *string            `json:"street_name" bson:"street_name"`
+	City      *string            `json:"city_name" bson:"city_name"`
+	Pincode   *string            `json:"pin_code" bson:"pin_code"`
+}
+
+type Order struct {
+	OrderID       primitive.ObjectID `bson:"_id"`
+	OrderCart     []ProductUser      `json:"order_list" bson:"order_list"`
+	OrderedAt     time.Time          `json:"ordered_at" bson:"ordered_at"`
+	Price         int                `json:"total_price" bson:"total_price"`
+	Discount      *int               `json:"discount" bson:"discount"`
+	PaymentMethod Payment            `json:"payment_method" bson:"payment_method"`
+}
+
+type Payment struct {
+	Digital bool
+	COD     bool
 }
