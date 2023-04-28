f79e3d04232b70075a457316c9fdef05eab0a025
Jateq <temirlan.eraly1@gmail.com>
2023-04-28 22:52:10 +0600

    SignUp or Login

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"github.com/Jateq/ecommerce/database"
 	"github.com/Jateq/ecommerce/models"
+	"github.com/Jateq/ecommerce/tokengen"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"net/http"
 	"time"
@@ -48,8 +51,8 @@ func SignUp() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
 		}
 
-		//count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
-		count, err = database.UserData(database.Client, "UserCollection").CountDocuments(ctx, bson.M{"email": user.Phone})
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		//count, err = database.UserData(database.Client, "UserCollection").CountDocuments(ctx, bson.M{"email": user.Phone})
 		defer cancel()
 		if err != nil {
 			log.Panic(err)
@@ -60,10 +63,60 @@ func SignUp() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone number is already used"})
 			return
 		}
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserID = user.ID.Hex()
+
+		token, refreshToken, _ := tokengen.TokenGenerator(*user.Email, *user.FirstName, *user.LastName, user.UserID)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.OrderStatus = make([]models.Order, 0)
+		_, insertErr := UserCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user did not created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "Successfully signed in!")
 	}
 }
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+
+		PasswordValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+		defer cancel()
+
+		if !PasswordValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println(msg)
+			return
+		}
+		token, refreshToken, _ := tokengen.TokenGenerator(*founduser.Email, *founduser.FirstName, *founduser.LastName, founduser.UserID)
+		tokengen.UpdateAllTokens(token, refreshToken, founduser.UserID)
+
+		c.JSON(http.StatusNotFound, founduser)
+	}
 }
 
 func AddProductAdmin() gin.HandlerFunc {
