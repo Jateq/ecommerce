026ac3463fa9be82e8a73229646eb4bfce5ea47c
Jateq <temirlan.eraly1@gmail.com>
2023-05-01 01:21:23 +0600

    aggregate

--- controllers/cart.go
+++ controllers/cart.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"github.com/Jateq/ecommerce/database"
+	"github.com/Jateq/ecommerce/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
@@ -94,7 +96,48 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 }
 
 func GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID := c.Query("id")
+		if userID == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "invalid id"})
+			c.Abort()
+			return
+		}
+		usertID, _ := primitive.ObjectIDFromHex(userID)
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		// still can't understand that contexts tbh
 
+		var filledCart models.User
+		err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usertID}}).Decode(&filledCart)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "not found")
+			return
+		}
+		// Aggregation queries
+		filterMatch := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: usertID}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"}, {Key: "total", Value: bson.D{primitive.E{Key: "$sum", Value: "$usercart.price"}}}}}}
+		pointCursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filterMatch, unwind, grouping})
+		if err != nil {
+			log.Println(err)
+		}
+
+		var listing []bson.M
+		if err = pointCursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing {
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledCart.UserCart)
+		}
+		ctx.Done()
+
+	}
 }
 
 func (app *Application) BuyFromCart() gin.HandlerFunc {
