1760de21230246f60d983bc38496d1f19d166a0a
Jateq <temirlan.eraly1@gmail.com>
2023-05-01 15:59:30 +0600

    middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	token "github.com/Jateq/ecommerce/tokengen"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Authorization header is not provided"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
