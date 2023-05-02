ac40179e17e9587616699a0765e8779083771e57
Jateq <temirlan.eraly1@gmail.com>
2023-05-02 22:19:11 +0600

    Add to Cart

--- database/cart.go
+++ database/cart.go
@@ -1,7 +1,13 @@
 package database
 
 import (
+	"context"
 	"errors"
+	"github.com/Jateq/ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 )
 
 var (
@@ -14,8 +20,32 @@ var (
 	ErrCantBuyItem        = errors.New("can't update the purchase")
 )
 
-func AddProductToCart() error {
+func AddProductToCart(ctx context.Contex, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	searchFromDB, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchFromDB.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
 
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "&push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 }
 
 func RemoveCartItem() error {
